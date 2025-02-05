dec6b49106e4420f453b98733b46470538f541bb
Hrasek <hosekmotor@seznam.cz>
2025-02-05 17:08:13 +0100

    feat: HandleClick in Player component.

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,23 @@
+import { useState } from 'react';
+
 export default function Player({ name, symbol }) {
+    const [isEditing, setIsEditing] = useState(false);
+
+    function handleEditClick(isEditing) {
+        setIsEditing(!isEditing)
+    }
+
+    let playerName = <span className="player-name">{name}</span>;
+    if (isEditing){
+        playerName = <input type="text" required></input>;    
+    }
     return (
         <li>
             <span>
-              <span className="player-name">{name}</span>
+                {playerName}
               <span className="player-symbol">{symbol}</span>
             </span>
-            <button>Edit</button>
+            <button onClick={() => handleEditClick(isEditing)}>{isEditing? 'Save' : 'Edit' }</button>
         </li>
     )
 }
