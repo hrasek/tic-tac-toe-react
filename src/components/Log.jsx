25fd101413a60e11abb023a155859c611c2ff8cd
Hrasek <hosekmotor@seznam.cz>
2025-02-07 11:05:06 +0100

    feat: Got rid of unnecessary state.

--- src/components/Log.jsx
+++ src/components/Log.jsx
@@ -8,9 +8,6 @@ export default function Log({ turns }){
    
 
     return <ol id="log">
-        {/* {turns.map((turn) => (
-          <li key={`${turn.square.row},${turn.square.col}`}>Player {turn.player} selected {turn.square.row},{turn.square.col} square</li>
-        ))} */}
-        {logText}
+            {logText}
          </ol>
 }
